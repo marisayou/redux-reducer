8ca482ac31b35d0644cc1f12d558df8929211610
Marisa You <marisayou2017@u.northwestern.edu>
2021-01-05 10:19:20 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            let friends = [...state.friends]
+            let idx = friends.findIndex(friend => friend.id === action.id)
+            friends.splice(idx, 1)
+
+            return { friends }
+        default:
+            return state
+    }
 }
